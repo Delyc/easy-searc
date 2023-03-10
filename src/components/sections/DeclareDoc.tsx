e1b0fbf530c37b78e683189660cfaf0938f37981
Delyc <d.twizeyima@alustudent.com>
2023-03-10 22:53:14 +0200

    Feat: Declare document designing

--- src/components/sections/DeclareDoc.tsx
+++ src/components/sections/DeclareDoc.tsx
@@ -2,12 +2,17 @@ import { useDeclareLostDocMutation } from "@/services/docsApiSlice";
 import { useState } from "react";
 import { toast } from "react-toastify";
 import Navbar from "../layouts/Navbar";
-import { SideArrow } from "../svgs/Icons";
+import { AtIcon, GmailIcon, LocationIcon, PhoneIcon, ProfileIcon, SideArrow } from "../svgs/Icons";
 import Button from "../ui/Button";
 import alerts from "../utils/Alert";
 import lostMan from '../../../public/assets/lost-man.svg'
+import man from '../../../public/assets/man.jpg'
 import Image from "next/image";
 import SectionWrapper from "../wrappers/SectionWrapper";
+import { SendIcon } from "../svgs/Icons";
+import card from '../../../public/assets/card.svg'
+import Footer from "../layouts/Footer";
+
 const DeclareDoc = () => {
 
     const initialData = {
@@ -42,12 +47,12 @@ const DeclareDoc = () => {
         <>
             <Navbar />
             <SectionWrapper>
-                <section className="flex justify-between items-center  mt-44 px-40">
-                    <div className="flex flex-col gap-5 px-10 items-center">
+                <section className="flex justify-between items-center mb-20 mt-44 px-32">
+                    <div className="flex flex-col gap-5  items-center">
                         <div className=" flex gap-2 items-center w-full justify-center">
-                            <Image src={lostMan} alt="man icon" className="h-[60vh] w-[30rem]" />
+                            <Image src={man} alt="man icon" className="h-[60vh] w-[30rem]" />
                             <div className="animate-bounce flex gap-2 items-center">Fill out the form <SideArrow /> </div>
-                       
+
                         </div>
                         <h1 className="text-dark-blue font-bold text-2xl w-4/5 text-center">Lost your document? <span className="text-primary-blue">No Worries!</span></h1>
 
@@ -55,16 +60,46 @@ const DeclareDoc = () => {
 
                     </div>
                     <form className="flex flex-col gap-2 w-2/5">
-                        <input name="names" onChange={inputHandler} placeholder="Your names" type="text" className="w-full  p-3 bg-white rounded-md border border-gray-border  text-gray-text" />
-                        <input name="phone" onChange={inputHandler} placeholder="Phone number" type="number" className="w-full  p-3 bg-white rounded-md border border-gray-border  text-gray-text" />
-                        <input name="email" onChange={inputHandler} placeholder="Email" type="email" className="w-full  p-3 bg-white rounded-md border border-gray-border  text-gray-text" />
-                        <input onChange={inputHandler} name="cardType" placeholder="Card type" type="text" className="w-full  p-3 bg-white rounded-md border border-gray-border  text-gray-text" />
-                        <input onChange={inputHandler} name="cardNumber" placeholder="Card Number" type="number" className="w-full  p-3 bg-white rounded-md border border-gray-border  text-gray-text" />
-                        <input onChange={inputHandler} name="location" placeholder="Location" type="text" className="w-full  p-3 bg-white rounded-md border border-gray-border  text-gray-text" />
-                        <Button text={isLoading ? "Sedning ..." : "Send"} className="rounded text-white" handleClick={handleSubmit} />
+                        <div className="flex items-center px-5 rounded-md border border-gray-border">
+                            <ProfileIcon />
+                            <input name="names" onChange={inputHandler} placeholder="Your names" type="text" className="w-full  p-3 bg-white outline-none  text-gray-text" />
+                        </div>
+
+                        <div className="flex  items-center px-5 rounded-md border border-gray-border ">
+                            <PhoneIcon />
+                            <input name="phone" onChange={inputHandler} placeholder="Phone number" type="number" className="w-full  p-3 bg-white outline-none text-gray-text" />
+
+                        </div>
+                        <div className="rounded-md border border-gray-border flex gap-2 items-center px-5">
+                            <AtIcon />
+                            <input name="email" onChange={inputHandler} placeholder="Email" type="email" className="w-full  p-3 bg-white outline-none text-gray-text" />
+
+                        </div>
+
+                        <div className="flex gap-2 px-5 items-center rounded-md border border-gray-border ">
+                            <Image src={card} alt="Card" height={20} width={20} />
+
+                            <input onChange={inputHandler} name="cardType" placeholder="Card type" type="text" className="w-full  p-3 bg-white outline-none text-gray-text" />
+
+                        </div>
+                        <div className="flex gap-2 px-5 items-center rounded-md border border-gray-border ">
+                            <Image src={card} alt="Card" height={20} width={20} />
+
+                            <input onChange={inputHandler} name="cardNumber" placeholder="Card type" type="text" className="w-full  p-3 bg-white outline-none text-gray-text" />
+
+                        </div>
+                        <div className="flex gap-2 items-center rounded-md border border-gray-border px-5">
+                            <LocationIcon />
+                            <input onChange={inputHandler} name="location" placeholder="Location" type="text" className="w-full  p-3 bg-white outline-none text-gray-text" />
+
+                        </div>
+
+                        <Button hasIcon={true} icon={<SendIcon />} text={isLoading ? "Sending ..." : "Send"} className="rounded text-white" handleClick={handleSubmit} />
                     </form>
                 </section>
             </SectionWrapper>
+
+            <Footer />
         </>
 
     );
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,11 +2,13 @@ interface Props{
     className ?: string,
     handleClick ?: any,
     text ?: string,
+    icon ? : any,
+    hasIcon : boolean
 }
 
-const Button = ({className, text, handleClick} : Props) => {
+const Button = ({className, text, handleClick, icon, hasIcon} : Props) => {
     return ( 
-        <button className={`${className} grid place-content-center bg-primary-blue px-5 py-4` }  onClick={handleClick}>{text}</button>
+        <button className={`${className} ${hasIcon ? "flex gap-2 items-center justify-center" : "grid place-content-center"} bg-primary-blue px-5 py-4 font-bold` } onClick={handleClick}>{text}{icon}</button>
      );
 }
  
